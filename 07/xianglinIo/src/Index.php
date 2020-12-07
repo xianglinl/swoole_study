fca26e2551c81f97cad5aa582296f222407ca3aa
xianglin <793101759@qq.com>
2020-12-07 20:50:38 +0800

    test: 💍 posix_getppid();

--- 07/xianglinIo/src/Index.php
+++ 07/xianglinIo/src/Index.php
@@ -11,6 +11,8 @@ class Index
 {
     public function index()
     {
+        echo posix_getppid().PHP_EOL;
+        sleep(10);
         echo date('Y-m-d H:i:s').PHP_EOL;
         return "这个是一个测试信息";
     }
