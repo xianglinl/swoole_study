468ae28269a848f6b015a6d469b1a414d4e29a54
xianglin <793101759@qq.com>
2020-12-07 20:52:12 +0800

    test: 💍 posix_getpid();

--- 07/xianglinIo/src/Index.php
+++ 07/xianglinIo/src/Index.php
@@ -5,15 +5,17 @@
  * Date：2020/11/26
  * Time：22:27
  */
+
 namespace xianglin\io;
 
 class Index
 {
     public function index()
     {
-        echo posix_getppid().PHP_EOL;
+        echo 'PPID====>' . posix_getppid() . PHP_EOL;
+        echo 'PID=====>' . posix_getpid() . PHP_EOL;
         sleep(10);
-        echo date('Y-m-d H:i:s').PHP_EOL;
+        echo date('Y-m-d H:i:s') . PHP_EOL;
         return "这个是一个测试信息";
     }
 }
